57621bbe59f4c61bbd3a522a14a5b67ba38f2739
flowjiyun <gyp2795@gmail.com>
2023-07-11 21:51:57 +0900

    feat: add user information validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 
@@ -8,8 +8,7 @@ export class AuthController {
     private authService: AuthService
   ) {}
   @Post('/signup')
-  signUp(
-    @Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+  signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.authService.signUp(authCredentialDto);
   }
 }

38b77c907fda021980297aa62c85d304eebf0090
flowjiyun <gyp2795@gmail.com>
2023-07-12 12:00:54 +0900

    feat: add user information when create board

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -3,6 +3,7 @@ import { Board } from "./board.entity"
 import { BoardStatus } from "./board-status.enum";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateBoardDto } from "./dto/create-board.dto";
+import { User } from "src/auth/user.entity";
 
 export class BoardRepository extends Repository<Board> {
   constructor(
@@ -12,13 +13,14 @@ export class BoardRepository extends Repository<Board> {
     super(boardRepository.target, boardRepository.manager, boardRepository.queryRunner);
   }
 
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+  async createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
     const { title, description } = createBoardDto;
 
     const board = this.boardRepository.create({
       title,
       description,
-      status: BoardStatus.PUBLIC
+      status: BoardStatus.PUBLIC,
+      user
     });
 
     await this.boardRepository.save(board);
